696a4cf456c0ab152b54f5ad2e4e336be82b29d6
Megan Gaebler & Perry Anderson <pair+missandry.peryanderson@devbootcamp.com>
2014-11-06 15:12:34 -0600

    added logic to games controller

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,49 @@
+class GamesController  < ApplicationController
+  require ApplicationHelper
+
+  def create
+    @game = Game.create(game_params)
+  end
+
+  def update
+    @user_input = game_params[:status_string]
+    @challenge = Challenge.find(game_params[:challenge_id])
+    @game = Game.find_by(user: current_user, challenge: @challenge)
+    @game.update_attribute(status_string: @user_input)
+
+    if @user_input == @challenge.solution
+      @game.update_attribute(completed: true)
+    end
+    move_robot(@user_input)
+  end
+
+  def move_robot(input)
+    case input
+    when input == "this.turnRight();"
+      @message = "I'm facing right"
+      # robot turns right
+      render json: {message: @message}, status: 200
+    when input == "this.turnLeft();"
+      @message = "I'm facing left"
+      # robot turns left
+      render json: {message: @message}, status: 200
+    when input == "this.forward();"
+      @message = "away!"
+      # robot moves forward
+      render json: {message: @message}, status: 200
+    when input == "this.backward();"
+      @message = "oh no!"
+      # robot moves backwards
+      render json: {message: @message}, status: 200
+    else
+      @message = "I don't understand you"
+    end
+
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit!
+  end
+end
