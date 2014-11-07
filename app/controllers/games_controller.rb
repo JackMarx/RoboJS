5394f80064e89e3fa5fa80dfd2b3cfa0cfe86365
Megan Gaebler & Perry Anderson <pair+missandry.peryanderson@devbootcamp.com>
2014-11-07 10:55:12 -0600

    multiline commands working hahahahahahahahahaaa

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,38 +19,54 @@ class GamesController  < ApplicationController
     @challenge = Challenge.find(params[:challenge_id])
     @game = Game.find_by(user: User.first, challenge: @challenge)
     @game.update_attribute(:status_string, @user_input)
-    p @game.status_string
 
     if @user_input == @challenge.solution
       @game.update_attribute(completed: true)
     end
-    move_robot(@user_input)
-
+    # p move_robot(@user_input)
+    execute_commands(@user_input)
     # render 'challenges/show'
+  end
+
+  def parse_commands(input)
+    input.split(/\r\n/)
+  end
+
+  def execute_commands(input)
+    input = @user_input
+    responded_arr = []
+    parsed_input = parse_commands(input)
+    parsed_input.each do |input|
+      p input
+      responded_arr << move_robot(input)
+    end
+    p responded_arr
+    @message = responded_arr.join("<br>")
+    render json: {message: @message, input: @user_input}, status: 200
   end
 
   def move_robot(input)
     @input = input
     case input
     when "this.turnRight();"
-      @message = "I'm facing right"
+      "I'm facing right"
+    # render json: {message: @message, input: @input}, status: 200
       # robot turns right
-      render json: {message: @message, input: @input}, status: 200
     when "this.turnLeft();"
-      @message = "I'm facing left"
+      "I'm facing left"
       # robot turns left
-      render json: {message: @message, input: @input}, status: 200
+      # render json: {message: @message, input: @input}, status: 200
     when "this.forward();"
-      @message = "away!"
+      "away!"
       # robot moves forward
-      render json: {message: @message, input: @input}, status: 200
+      # render json: {message: @message, input: @input}, status: 200
     when "this.backward();"
-      @message = "oh no!"
+      "oh no!"
       # robot moves backwards
-      render json: {message: @message, input: @input}, status: 200
+      # render json: {message: @message, input: @input}, status: 200
     else
-      p @message = "I don't understand you"
-      render json: {message: "I don't know what you mean", input: @input}, status: 200
+      "I don't understand you"
+      # render json: {message: "I don't know what you mean", input: @input}, status: 200
     end
   end
 
