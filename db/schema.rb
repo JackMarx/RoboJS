07b42f53174ac93138340cbf9402b30586ce9980
Megan Gaebler & Perry Anderson <pair+missandry.peryanderson@devbootcamp.com>
2014-11-07 17:57:18 -0600

    added fabric library

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106201852) do
+ActiveRecord::Schema.define(version: 20141106211240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
