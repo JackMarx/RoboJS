315ffd4e47cedffe1d6d7628fd02ab2650caaf56
Megan Gaebler <megan.gaebler@gmail.com>
2014-11-07 21:13:14 -0600

    started spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe "Game" do
   let(:challenge){ instance_double("Challenge", id: 1) }
   let(:user){ instance_double("User", id: 1) }
-  let(:lotr){ Game.new(user_id: 1, challenge_id: 1) }
+  let(:lotr){ Game.new(user_id: 1, challenge_id: 1, instructions: "L,F1,F2,L,B2,R,B1") }
 
   allow(challenge).to receive(:games).and_return([lotr])
-  allow(user).to receive(:games).and_return([lotr])
+  allow(user).to receive(:games).and_return([lotr, ghostbusters])
   
-  
+
 
 end
