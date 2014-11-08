fc6e3c2a7f72fb2ca718a9e88a44d56d61ea55c3
Megan Gaebler <megan.gaebler@gmail.com>
2014-11-08 14:27:58 -0600

    some real challenges take 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,11 @@ challenge2 = Challenge.create!(title: "Going the Distance",
                                solution: "rupert.moveForward();,rupert.moveForward();",
                                hint: "Try one of these: rupert.moveRight();, rupert.moveLeft();, rupert.moveForward();")
 
-
 challenge3 = Challenge.create!(title: "Argue with Me",
                                description: "It's a little tedious writing that function over and over, right? We can make the same thing happen by passing an argument to the moveForward(); function.\n\nYou might know an argument as an unpleasant shouting match between two people, but in JavaScript-land, it only means something you put between the parentheses at the end of the function.\n\nFor example, if you type rupert.moveForward(2);, Rupert will move forward twice.",
                                tutorial: "",
                                solution: "rupert.moveForward(2);",
                                hint: "Try one of these: rupert.moveRight();, rupert.moveLeft();, rupert.moveForward();")
-
 
 challenge4 = Challenge.create!(title: "Defeating the Zoolander Curse",
                                description: "Great! Now that Rupert has moved forward, let's see if we can get him to turn left before moving forward again.",
