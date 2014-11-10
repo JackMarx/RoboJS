59f66883c66b85d6423342fdcdfc0a1d11de3510
Christian Stanfield <christian.stanfield@gmail.com>
2014-11-10 00:06:02 -0600

    basic robot logic

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,25 @@
+class RobotsController < ApplicationController
+
+  def move_robot
+    action_array = robot_instructions.split(",")
+
+    action_array.each do |action|
+      case action
+      when 'F' then event = 'forward'
+      when 'L' then event = 'left'
+      when 'R' then event = 'right'
+      when 'S' then event = 'stop'
+      else
+        multiple_forward
+      end
+    Pusher.trigger('robot_channel', event, {:some => 'data'})
+    end
+  end
+
+  def multiple_forward
+    num = (action[1]..action[-1]).to_a.join.to_i
+    num.times do
+      Pusher.trigger('robot_channel', 'forward', {:some => 'data'})
+    end
+  end
+end
