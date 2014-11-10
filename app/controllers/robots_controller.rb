436a70e008c6f43c42fde37acbef879f16d9c288
Christian Stanfield <christian.stanfield@gmail.com>
2014-11-10 09:11:55 -0600

    updated robot logic

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,6 +1,6 @@
 class RobotsController < ApplicationController
 
-  def move_robot
+  def move_robot(robot_instructions)
     action_array = robot_instructions.split(",")
 
     action_array.each do |action|
@@ -9,17 +9,15 @@ class RobotsController < ApplicationController
       when 'L' then event = 'left'
       when 'R' then event = 'right'
       when 'S' then event = 'stop'
-      else
-        multiple_forward
       end
     Pusher.trigger('robot_channel', event, {:some => 'data'})
     end
   end
 
-  def multiple_forward
-    num = (action[1]..action[-1]).to_a.join.to_i
-    num.times do
-      Pusher.trigger('robot_channel', 'forward', {:some => 'data'})
-    end
-  end
+  # def multiple_forward
+  #   num = (action[1]..action[-1]).to_a.join.to_i
+  #   num.times do
+  #     Pusher.trigger('robot_channel', 'forward', {:some => 'data'})
+  #   end
+  # end
 end
