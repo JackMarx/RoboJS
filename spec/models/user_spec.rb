d6b397a5b9c727e3cd02a13b787ba82b2fe73e04
Megan Gaebler <megan.gaebler@gmail.com>
2014-11-10 14:29:27 -0600

    user models done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,23 +2,24 @@ require 'rails_helper'
 
 
 describe User do 
+  it { should have_many(:games) }
+  it { should have_many(:badges) }
+  it { should have_secure_password }
 
-  it "has a valid factory" do
-    FactoryGirl.create(:user).should be_valid
+  it "is invalid without a username" do 
+    expect(FactoryGirl.build(:user, username: nil)).not_to be_valid 
   end
-  # it "has a valid factory" do
-  #   Factory.create(:user).should be_valid 
-  # end
 
-  # let(:rupert) { FactoryGirl.create :user }
+  it "is invalid without a password" do 
+    expect(FactoryGirl.build(:user, password: nil)).not_to be_valid 
+  end 
 
-  # it "is invalid without a username" do 
-  #   expect(rupert.username == nil).should_not be_valid 
-  # end
+  it "is invalid without a password confirmation" do
+    expect(FactoryGirl.build(:user, password_confirmation: nil)).not_to be_valid
+  end
 
-  # it "is invalid without a password" do 
-  #   FactoryGirl.build(:user, password: nil).should_not be_valid 
-  # end 
-
-  # it "returns a contact's full name as a string" 
+  it "is invalid without a matching password" do
+    expect(FactoryGirl.build(:user, password_confirmation: "secret")).not_to be_valid
+    expect(FactoryGirl.build(:user, password: "secret")).not_to be_valid
+  end
 end
