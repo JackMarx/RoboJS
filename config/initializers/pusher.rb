63672e683232f703a7ad43c9b182d990e59b1b5f
Perry Anderson <peryanderson@gmail.com>
2014-11-09 23:04:38 -0600

    uncommented pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,11 +1,11 @@
-# require 'pusher'
+require 'pusher'
 
-# Pusher.url = "http://a32b9aab7196cd0862d2:0d0798a8039ff675fdc7@api.pusherapp.com/apps/95703"
+Pusher.url = "http://a32b9aab7196cd0862d2:0d0798a8039ff675fdc7@api.pusherapp.com/apps/95703"
 
-# pusher = YAML.load_file(File.join(Rails.application.root, 'config/pusher.yml'))
+pusher = YAML.load_file(File.join(Rails.application.root, 'config/pusher.yml'))
 
-# Pusher.app_id = pusher['app_id']
-# Pusher.key = pusher['key']
-# Pusher.secret = pusher['secret']
+Pusher.app_id = pusher['app_id']
+Pusher.key = pusher['key']
+Pusher.secret = pusher['secret']
 
-# Pusher.logger = Rails.logger
+Pusher.logger = Rails.logger
