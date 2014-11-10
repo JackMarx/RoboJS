7584228d0c974a6e72107d64683a8320fc0b9059
Christian Stanfield <christian.stanfield@gmail.com>
2014-11-10 13:26:57 -0600

    added model method

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -13,4 +13,14 @@ class Challenge < ActiveRecord::Base
       challenge6: /for\(var i=1;i<4;i\+\+\){\\nrupert.moveForward(\(\));rupert.turnLeft(\(\));\\n}/
     }
   end
+
+  def self.current_challenges
+    Challenge.all.map do |challenge|
+      if challenge.users.include? current_user
+        {game: current_user.games.find_by(challenge: challenge).attributes, challenge_id: challenge.id}
+      else
+        {user_id: current_user.id.to_i, challenge_id: challenge.id.to_i, game: Game.new.attributes}
+      end
+    end
+  end
 end
