53ed9f4e05fe2af057151d03babd2c123910a76a
Megan Gaebler <megan.gaebler@gmail.com>
2014-11-10 19:48:23 -0600

    tryna get that game creation logic more solid

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -9,14 +9,14 @@ include ApplicationHelper
 
     if @challenge != Challenge.all[-1]
       @next_challenge = Challenge.find(@challenge.id + 1)
+      @next_game = current_user.games.find_or_create_by(challenge_id: @challenge.id + 1, user_id: current_user.id ).attributes
     end
 
     if @challenge != Challenge.first
       @previous_challenge = Challenge.find(@challenge.id - 1)
+      @prev_game = current_user.games.find_or_create_by(challenge_id: @challenge.id - 1, user_id: current_user.id ).attributes
     end
 
     @game = Game.find_by(challenge: @challenge, user: current_user)
-    @next_game = current_user.games.find_or_create_by(challenge_id: @challenge.id + 1, user_id: current_user.id ).attributes
-    @prev_game = current_user.games.find_or_create_by(challenge_id: @challenge.id - 1, user_id: current_user.id ).attributes
   end
 end
