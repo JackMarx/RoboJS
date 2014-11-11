8bc39a6cbccb3292b7eff2a6082985f409e851d7
Megan Gaebler <megan.gaebler@gmail.com>
2014-11-10 22:44:29 -0600

    centered status circles and got some colors dependent on game status

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -20,10 +20,22 @@ class Challenge < ActiveRecord::Base
   def self.current_challenges(user)
     Challenge.all.map do |challenge|
       if challenge.users.include? user
-        {game: user.games.find_by(challenge: challenge).attributes, challenge_id: challenge.id}
+        { game: user.games.find_by(challenge: challenge).attributes, challenge_id: challenge.id }
       else
-        {user_id: user.id.to_i, challenge_id: challenge.id.to_i, game: Game.new.attributes}
+        { user_id: user.id.to_i, challenge_id: challenge.id.to_i, game: Game.new.attributes }
       end
     end
+  end
+
+  def user_game(user)
+    if self.users.pluck(:user_id).include? user.id
+      return user.games.find_by(challenge_id: self.id)
+    else
+      return Game.new
+    end
+  end
+
+  def game_status(user)
+    self.user_game(user).complete_or_incomplete
   end
 end
