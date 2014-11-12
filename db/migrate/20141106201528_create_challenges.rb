b0ea5fe3b4c01cec14f08fa67755ecd64a94550b
Christian Stanfield <christian.stanfield@gmail.com>
2014-11-12 15:49:31 -0600

    badges popup now

--- db/migrate/20141106201528_create_challenges.rb
+++ db/migrate/20141106201528_create_challenges.rb
@@ -2,7 +2,7 @@ class CreateChallenges < ActiveRecord::Migration
   def change
     create_table :challenges do |t|
       t.string    :title
-      t.text      :tutorial
+      t.boolean   :tutorial
       t.string    :solution
       t.text      :description
       t.string    :hint
